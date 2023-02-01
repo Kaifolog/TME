5299d7bea76836531928fa18d2e81a3a0404e8b5
Kaifolog <andreypracticekpi@gmail.com>
2023-02-01 08:41:10 +0200

    refactor: tests/*

--- tests/machine/MachineTests.cpp
+++ tests/machine/MachineTests.cpp
@@ -1,91 +1,94 @@
+#include <fstream>
 #include <gtest/gtest.h>
 #include <string>
-#include <fstream>
 
 #include "../../src/machine/TuringMachine.hpp"
 #include "../../src/translator/translator.hpp"
 
 TEST(ExecutionTest, BasicTest)
 {
-        Machine::TuringMachine tm;
+    Machine::TuringMachine tm;
 
-        // preparing test
-        ofstream datasec;
-        datasec.open("datasection.tmp");
-        datasec << "|lambda|";
-        datasec.close();
+    // preparing test
+    std::ofstream datasec;
+    datasec.open("datasection.tmp");
+    datasec << "|lambda|";
+    datasec.close();
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/machine/BasicTest.txt");
-        ASSERT_NO_THROW(tm.execute(pn, 0));
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/machine/BasicTest.txt");
+    ASSERT_NO_THROW(tm.execute(pn, 0));
 
-        // preparing test
-        string result;
-        ifstream output("../tests/machine/BasicTest_out.txt");
-        getline(output, result);
-        ASSERT_STREQ("1 1 1 |lambda| ", result.c_str());
+    // preparing test
+    std::string result;
+    std::ifstream output("../tests/machine/BasicTest_out.txt");
+    getline(output, result);
+    ASSERT_STREQ("1 1 1 |lambda| ", result.c_str());
 }
 
 TEST(ExecutionTest, LambdaTest)
 {
-        Machine::TuringMachine tm;
+    Machine::TuringMachine tm;
 
-        // preparing test
-        ofstream datasec;
-        datasec.open("datasection.tmp");
-        datasec << "|lambda|";
-        datasec.close();
+    // preparing test
+    std::ofstream datasec;
+    datasec.open("datasection.tmp");
+    datasec << "|lambda|";
+    datasec.close();
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/machine/BasicTest.txt");
-        ASSERT_NO_THROW(tm.execute(pn, 1));
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/machine/BasicTest.txt");
+    ASSERT_NO_THROW(tm.execute(pn, 1));
 
-        // preparing test
-        string result;
-        ifstream output("../tests/machine/BasicTest_out.txt");
-        getline(output, result);
-        ASSERT_STREQ("1 1 1 | | ", result.c_str());
+    // preparing test
+    std::string result;
+    std::ifstream output("../tests/machine/BasicTest_out.txt");
+    getline(output, result);
+    ASSERT_STREQ("1 1 1 | | ", result.c_str());
 }
 
 TEST(ExecutionTest, HardTest)
 {
-        Machine::TuringMachine tm;
+    Machine::TuringMachine tm;
 
-        // preparing test
-        ofstream datasec;
-        datasec.open("datasection.tmp");
-        datasec << "|1| 0 1 0 1 0 1 0 0 1 0 1 0 1 0 1";
-        datasec.close();
+    // preparing test
+    std::ofstream datasec;
+    datasec.open("datasection.tmp");
+    datasec << "|1| 0 1 0 1 0 1 0 0 1 0 1 0 1 0 1";
+    datasec.close();
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../examples/ZhegalkinOfDualFunc.txt");
-        ASSERT_NO_THROW(tm.execute(pn, 1));
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../examples/ZhegalkinOfDualFunc.txt");
+    ASSERT_NO_THROW(tm.execute(pn, 1));
 
-        // preparing test
-        string result;
-        ifstream output("../examples/ZhegalkinOfDualFunc_out.txt");
-        getline(output, result);
-        ASSERT_STREQ("1 0 1 0 1 0 1 0 0 1 0 1 0 1 0 1 =                                   x4 + |x1|                                                                                                                                                             ", result.c_str());
+    // preparing test
+    std::string result;
+    std::ifstream output("../examples/ZhegalkinOfDualFunc_out.txt");
+    getline(output, result);
+    ASSERT_STREQ("1 0 1 0 1 0 1 0 0 1 0 1 0 1 0 1 =                                   x4 + |x1|                        "
+                 "                                                                                                     "
+                 "                                ",
+                 result.c_str());
 }
 
 TEST(DebuggingTest, LambdaTest)
 {
-        Machine::TuringMachine tm;
-        Machine::MachineState result;
+    Machine::TuringMachine tm;
+    Machine::MachineState result;
 
-        ofstream datasec;
-        datasec.open("datasection.tmp");
-        datasec << "|lambda|";
-        datasec.close();
+    std::ofstream datasec;
+    datasec.open("datasection.tmp");
+    datasec << "|lambda|";
+    datasec.close();
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/machine/BasicTest.txt");
-        ASSERT_NO_THROW(tm.lazyStart(pn, 0));
-        ASSERT_NO_THROW(result = tm.lazyDebug(););
-        ASSERT_EQ(result.line, "6");
-        ASSERT_NO_THROW(result = tm.lazyDebug(););
-        ASSERT_EQ(result.current_state, "end");
-        // ASSERT_NO_THROW(result = tm.lazyDebug();); // remember it for writing destructive test
-        // ASSERT_EQ(result.current_state, "");
-        tm.lazyFinalize();
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/machine/BasicTest.txt");
+    ASSERT_NO_THROW(tm.lazyStart(pn, 0));
+    ASSERT_NO_THROW(result = tm.lazyDebug(););
+    ASSERT_EQ(result.line, "6");
+    ASSERT_NO_THROW(result = tm.lazyDebug(););
+    ASSERT_EQ(result.current_state, "end");
+    // ASSERT_NO_THROW(result = tm.lazyDebug();); // remember it for writing destructive test
+    // ASSERT_EQ(result.current_state, "");
+    tm.lazyFinalize();
 }
--- tests/translator/AnalyserTests.cpp
+++ tests/translator/AnalyserTests.cpp
@@ -1,135 +1,135 @@
+#include <fstream>
 #include <gtest/gtest.h>
 #include <string>
-#include <fstream>
 
 #include "../../src/translator/translator.hpp"
 
 TEST(ParsingTest, BasicTest)
 {
-        Translator translator;
+    translator::Translator translator;
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/translator/BasicTest.txt");
-        ASSERT_NO_THROW(translator.parse(pn));
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/translator/BasicTest.txt");
+    ASSERT_NO_THROW(translator.parse(pn));
 }
 
 TEST(ParsingTest, BasicFailTest)
 {
-        Translator translator;
+    translator::Translator translator;
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/translator/BasicFailTest.txt");
-        string output;
-        try
-        {
-                translator.parse(pn);
-        }
-        catch (const char *message)
-        {
-                output = message;
-        }
-        catch (string message)
-        {
-                output = message;
-        }
-        ASSERT_STREQ("parse error at line #6", output.c_str());
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/translator/BasicFailTest.txt");
+    std::string output;
+    try
+    {
+        translator.parse(pn);
+    }
+    catch (const char *message)
+    {
+        output = message;
+    }
+    catch (std::string message)
+    {
+        output = message;
+    }
+    ASSERT_STREQ("parse error at line #6", output.c_str());
 }
 
 TEST(ParsingTest, HardTest)
 {
-        Translator translator;
+    translator::Translator translator;
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../examples/ZhegalkinOfDualFunc.txt");
-        ASSERT_NO_THROW(translator.parse(pn));
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../examples/ZhegalkinOfDualFunc.txt");
+    ASSERT_NO_THROW(translator.parse(pn));
 }
 
 TEST(ParsingTest, SectionDataTest)
 {
-        Translator translator;
+    translator::Translator translator;
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/translator/SectionDataTest.txt");
-        string output;
-        try
-        {
-                translator.parse(pn);
-        }
-        catch (const char *message)
-        {
-                output = message;
-        }
-        catch (string message)
-        {
-                output = message;
-        }
-        ASSERT_STREQ("SECTION parse error at line #1", output.c_str());
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/translator/SectionDataTest.txt");
+    std::string output;
+    try
+    {
+        translator.parse(pn);
+    }
+    catch (const char *message)
+    {
+        output = message;
+    }
+    catch (std::string message)
+    {
+        output = message;
+    }
+    ASSERT_STREQ("SECTION parse error at line #1", output.c_str());
 }
 
 TEST(ParsingTest, SectionTextTest)
 {
-        Translator translator;
+    translator::Translator translator;
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/translator/SectionTextTest.txt");
-        string output;
-        try
-        {
-                translator.parse(pn);
-        }
-        catch (const char *message)
-        {
-                output = message;
-        }
-        catch (string message)
-        {
-                output = message;
-        }
-        ASSERT_STREQ("SECTION parse error at line #4", output.c_str());
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/translator/SectionTextTest.txt");
+    std::string output;
+    try
+    {
+        translator.parse(pn);
+    }
+    catch (const char *message)
+    {
+        output = message;
+    }
+    catch (std::string message)
+    {
+        output = message;
+    }
+    ASSERT_STREQ("SECTION parse error at line #4", output.c_str());
 }
 
 TEST(AnalysisTest, StartTest)
 {
-        Translator translator;
+    translator::Translator translator;
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/translator/StartTest.txt");
-        string output;
-        try
-        {
-                translator.parse(pn);
-                translator.analyse(pn);
-        }
-        catch (const char *message)
-        {
-                output = message;
-        }
-        catch (string message)
-        {
-                output = message;
-        }
-        ASSERT_STREQ("ANALYSER FATAL ERROR! There are no \"start\" command.", output.c_str());
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/translator/StartTest.txt");
+    std::string output;
+    try
+    {
+        translator.parse(pn);
+        translator.analyse(pn);
+    }
+    catch (const char *message)
+    {
+        output = message;
+    }
+    catch (std::string message)
+    {
+        output = message;
+    }
+    ASSERT_STREQ("ANALYSER FATAL ERROR! There are no \"start\" command.", output.c_str());
 }
 
 TEST(AnalysisTest, EndTest)
 {
-        Translator translator;
+    translator::Translator translator;
 
-        // you should start it from ./TME/build
-        Tools::ProjectName pn("../tests/translator/EndTest.txt");
-        string output;
-        try
-        {
-                translator.parse(pn);
-                translator.analyse(pn);
-        }
-        catch (const char *message)
-        {
-                output = message;
-        }
-        catch (string message)
-        {
-                output = message;
-        }
-        ASSERT_STREQ("ANALYSER FATAL ERROR! There are no \"end\" command.", output.c_str());
+    // you should start it from ./TME/build
+    Tools::ProjectName pn("../tests/translator/EndTest.txt");
+    std::string output;
+    try
+    {
+        translator.parse(pn);
+        translator.analyse(pn);
+    }
+    catch (const char *message)
+    {
+        output = message;
+    }
+    catch (std::string message)
+    {
+        output = message;
+    }
+    ASSERT_STREQ("ANALYSER FATAL ERROR! There are no \"end\" command.", output.c_str());
 }
