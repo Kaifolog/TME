2db45fd7ed0ad1ca0efc0f08616c68d94d3192e9
Kaifolog <andreypracticekpi@gmail.com>
2023-02-06 06:33:26 +0200

    change(gui): the theme has built in

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include <iostream>
 
+#include "themes.h"
 #include <QApplication>
 
 INITIALIZE_EASYLOGGINGPP
@@ -10,15 +11,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    // sets style
-    QFile styleSheetFile("./MoonlightDark.qss");
-    styleSheetFile.open(QFile::ReadOnly);
-    if (styleSheetFile.isOpen())
-    {
-        QString styleSheet = QLatin1String(styleSheetFile.readAll());
-        a.setStyleSheet(styleSheet);
-        w.isDarkMode = 1;
-    }
+    a.setStyleSheet(themes::MoonlightTheme);
+    w.isDarkMode = true;
 
     w.show();
     return a.exec();
